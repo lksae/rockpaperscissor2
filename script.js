ed0fa124ef6ca8243790b246500b58752b8e2e80
Lksae <matthiasschmitz21@gmail.com>
2024-05-29 19:08:22 +0200

    Finish computer choie function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let computerChoice = "not defined";
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        computerChoice = "Rock";
+    } else if (randomNumber < 0.66){
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissor";
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
